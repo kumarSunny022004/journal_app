0bfe88ce7aac0159909688cf867f49cf96440367
KUMARSUNNY <ks9107163@gmail.com>
2025-06-10 17:37:45 +0530

    LomboK

--- journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,6 +1,8 @@
 package net.engineeringdigest.journalApp.entity;
 
 
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,6 +12,14 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Slf4j
+@EqualsAndHashCode
+@Builder
 public class JournalEntry {
 
 
@@ -22,36 +32,4 @@ public class JournalEntry {
 
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
