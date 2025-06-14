c524ac65ba0633394e0ec69131df7653d11d175f
KUMARSUNNY <ks9107163@gmail.com>
2025-06-14 13:23:07 +0530

    resolved the bug in admin controller

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/AdminController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/AdminController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +26,8 @@ public class AdminController {
         }
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+    @PostMapping("/create_new_admin")
     public void createnewAdmin(@RequestBody Users user){
         userService.saveNewAdmin(user);
     }
