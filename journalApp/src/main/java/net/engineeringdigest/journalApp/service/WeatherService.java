e99f35854888542c5798acc23eeabadbe071e987
KUMARSUNNY <ks9107163@gmail.com>
2025-06-16 13:40:38 +0530

    Value Annotation

--- journalApp/src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -2,15 +2,19 @@ package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.apiResponse.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
 
-    private static final String apiKey = "292501740da6fdafcdc371949308142f";
+    @Value("${weather.api.key}")
+    private  String apiKey;
+
     private static final String  API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
